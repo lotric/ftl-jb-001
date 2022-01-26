a7a2615284566f2c07c465386f02f78380ea0fc3
Mikhail_Vdovin <misha.vdovin.00@gmail.com>
2022-01-26 17:43:59 +0300

    L006
    static/instance methods example

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import com.company.animals.Cat;
+import com.company.animals.Dog;
 
 public class Main {
 
@@ -17,5 +18,20 @@ public class Main {
 
         Cat cat = new Cat("Barsik", 20);
         System.out.println(cat.jumpHeight());
+
+
+        Dog dog = new Dog();
+
+
+        dog.goToStick(14);
+        dog.whereTheDog();
+
+        dog.goToStick(20);
+        dog.whereTheDog();
+
+        dog.goToStick(5);
+        dog.whereTheDog();
+
+        Dog homelessDog = Dog.ofHomeless(100);
     }
 }

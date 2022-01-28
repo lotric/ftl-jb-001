294a185059337521cadbd835e58f2aeebbf6d9bc
Mikhail_Vdovin <misha.vdovin.00@gmail.com>
2022-01-28 20:09:16 +0300

    task002

---
+++ src/tasks/task002/Main.java
@@ -0,0 +1,11 @@
+package tasks.task002;
+
+public class Main {
+
+    public static void main(String[] args){
+        for(int i = 5; i > 0 ; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}

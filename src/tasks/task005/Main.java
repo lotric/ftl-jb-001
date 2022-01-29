75ffd4179c5845437588af8a30dc88519eae3b3a
Mikhail_Vdovin <misha.vdovin.00@gmail.com>
2022-01-29 16:24:10 +0300

    task005

---
+++ src/tasks/task005/Main.java
@@ -0,0 +1,14 @@
+package tasks.task005;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int number = 7;
+
+        for(int i = 1; i <= 14; i++){
+            System.out.print(number * i + " ");
+        }
+        System.out.println();
+    }
+}

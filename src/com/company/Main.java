c13b7a7d0f01576b87c848fd41a4671e500369b8
Mikhail_Vdovin <misha.vdovin.00@gmail.com>
2022-02-03 14:35:52 +0300

    L020
    Parse examples

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 import com.company.L013.SwitchExample;
 import com.company.L019.StringExample;
+import com.company.L020.TypeConversion;
 import com.company.animals.Cat;
 import com.company.animals.Dog;
 import com.company.animals.Duck;
@@ -101,5 +102,7 @@ public class Main {
         }
 
         StringExample.run();
+
+        TypeConversion.run();
     }
 }

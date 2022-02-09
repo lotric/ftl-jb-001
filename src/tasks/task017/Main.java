0abc1d511a4934c0b52363f630bca840b3827455
Mikhail_Vdovin <misha.vdovin.00@gmail.com>
2022-02-09 15:30:54 +0300

    task017

---
+++ src/tasks/task017/Main.java
@@ -0,0 +1,26 @@
+package tasks.task017;
+
+public class Main {
+
+    public static double func(double x){
+        return Math.cos(Math.pow(x, 5)) + Math.pow(x, 4) - 345.3 * x - 23;
+    }
+
+    public static double solve(double start, double end){
+        if(end - start <= 0.001){
+            return start;
+        }
+
+        double x = start + (end - start) / 2;
+
+        if(func(start) * func(x) > 0){
+            return solve(x, end);
+        } else {
+            return solve(start, x);
+        }
+    }
+
+    public static void main(String[] args){
+        System.out.println(solve(0, 10));
+    }
+}
